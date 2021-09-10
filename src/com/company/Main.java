0ce283bfbf425b222908f60675f6c771d651384e
Dnguyen99 <dylannguyen@Knights.ucf.edu>
2021-09-09 20:47:51 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the input string? ");
+        String str = input.nextLine();
+        int count = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) != ' ')
+                count++;
+        }
+//Displays the total number of characters present in the given string
+        System.out.println("\n"+str+" has "+count+" characters.");
+
+    }
+}
